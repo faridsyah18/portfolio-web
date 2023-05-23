488ea4a09bf30593d9cb034a8c8e452d55a2f262
Faridsyah <faridsyahalmuluk2@gmail.com>
2023-05-23 20:58:29 +0700

    23 mei 2023

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-let welcome = document.querySelector('.welcome-h1-desktop')
-let mine = document.querySelector('.mine-desktop')
+// Mengambil semua elemen <a> di dalam .nav-desktop-left
+const navLinks = document.querySelectorAll('.nav-desktop-left a');
+
+// Mengambil semua elemen <h1>
+const headings = document.querySelectorAll('.judul .sub-header h1');
+
+// Mendaftarkan event listener untuk setiap elemen <a>
+navLinks.forEach((link, index) => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); // Menghentikan perilaku default dari tautan
+
+    // Menghapus kelas .active dari semua elemen <a>
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+
+    // Menambahkan kelas .active pada elemen <a> yang diklik
+        this.classList.add('active');
+    });
+});
